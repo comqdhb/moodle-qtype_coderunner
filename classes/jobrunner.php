6568970d9cfe581e328b830e9465288cc9730b7b
David Bowes <d.h.bowes@herts.ac.uk>
2017-01-25 09:39:40 +0000

    remove twig rendering of the expected test answer

--- classes/jobrunner.php
+++ classes/jobrunner.php
@@ -83,19 +83,7 @@ class qtype_coderunner_jobrunner {
             'STUDENT' => new qtype_coderunner_student($USER)
          );
 
-         $outcome = null;
-  
-         
-         foreach ($this->testcases as $testcase) {
-           //apply Twig variables to the testcase
- 	   try {
-		$testcase->expected  =   $this->twig->render($testcase->expected,$this->templateparams);
-           } catch (Exception $e) {
-             $testcase->expected = $e->getMessage();
-            //needs something to deal with not being able to render correctly like...
-           }
-         }
-
+        $outcome = null;
         if ( $question->get_is_combinator() and $this->has_no_stdins()) {
             $outcome = $this->run_combinator();
         } 
